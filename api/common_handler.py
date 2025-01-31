ffcf4783ce8773520351b321375cc3ac73f35046
root <xoliqbediyevbehruz@gmail.com>
2025-01-31 15:25:46 +0100

    changes

--- api/common_handler.py
+++ api/common_handler.py
@@ -10,7 +10,6 @@ from api.login_handler import get_current_user_from_token
 
 common_router = APIRouter()
 
-#create upcoming expenses and maybe incomes for the company handlers
 @common_router.get('/list-income-expected-val', response_model=List[schemas.ShowExpectedValue])
 async def get_all_income_val(db:AsyncSession = Depends(session.get_db), current_user:models.Employees=Depends(get_current_user_from_token)):
     return await common_action._get_all_income_expected_value(session=db)
@@ -36,9 +35,8 @@ async def update_expected_value(expected_avl_id:int ,updated_val_params:schemas.
     body = updated_val_params.model_dump(exclude_none=True)
     return await common_action._update_expected_value(session=db, body=body, expected_avl_id=expected_avl_id)
 
-#in this right here you can see only task related handlers
 @common_router.get('/get-all-new-task', response_model=List[schemas.ShowNewTask])
-async def create_new_task(task_id:Optional[int]=None ,status:Optional[str]=None,db:AsyncSession = Depends(session.get_db),
+async def get_new_task(task_id:Optional[int]=None ,status:Optional[str]=None,db:AsyncSession = Depends(session.get_db),
                           current_user:models.Employees=Depends(get_current_user_from_token)):
     return await common_action._(status=status, session=db,task_id=task_id)
 
