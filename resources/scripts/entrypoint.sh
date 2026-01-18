a838b9cda4fdcc3b3cccb519c48d4cbe0a592d5a
xoliqberdiyev <xoliqberdiyevbehruz12@gmail.com>
2026-01-18 16:11:38 +0500

    fuxk

--- resources/scripts/entrypoint.sh
+++ resources/scripts/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-echo "⏳ Running Alembic migrations..."
-alembic upgrade head
+# echo "⏳ Running Alembic migrations..."
+# alembic upgrade head
 
 echo "🚀 Starting Uvicorn..."
 exec uvicorn main:app --host 0.0.0.0 --port 8000
